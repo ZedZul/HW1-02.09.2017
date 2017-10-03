9bf1423b8e22641b474b22ff1238c46c5d6067c0
ZedZul <faqproshival@yandex.ru>
2017-10-03 17:46:31 +0300

    Cherry Pick log log fixed
    # Conflicts:
    #	app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java

--- app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
+++ app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
@@ -2,13 +2,19 @@ package com.zedzul.github.hw1_02092017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG_GOOD = "GOOD";
+    public static final String MSG_ALL_RIGHT = "ALL RIGHT";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //some changes
+        Log.d(TAG_GOOD, MSG_ALL_RIGHT);
     }
 
     public void someFileWriter(){
