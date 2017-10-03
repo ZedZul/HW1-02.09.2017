00ca654e8be8bef2e83e66b95b7a9f19741cdc2b
ZedZul <faqproshival@yandex.ru>
2017-10-03 18:23:15 +0300

    update master, test rebase

--- app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
+++ app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG_GOOD, MSG_ALL_RIGHT);
         //update master
         //some feature added
+        //update master 2
     }
 
     public void someFileWriter(){
