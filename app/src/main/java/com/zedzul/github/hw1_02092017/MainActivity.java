67ab1b5ae8a302bc01140f118b50990e7e92b670
ZedZul <faqproshival@yandex.ru>
2017-10-03 17:46:31 +0300

    log fixed

--- app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
+++ app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
@@ -6,12 +6,15 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG_GOOD = "GOOD";
+    public static final String MSG_ALL_RIGHT = "ALL RIGHT";
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //some changes
-        Log.d("GOOD","ALL RIGHT");
+        Log.d(TAG_GOOD, MSG_ALL_RIGHT);
     }
 
     public void someFileWriter(){
