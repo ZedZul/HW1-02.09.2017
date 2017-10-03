65f19969f1be58684d5e8ffa2178a79d2cd06a5a
ZedZul <faqproshival@yandex.ru>
2017-10-03 17:57:52 +0300

    update master

--- app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
+++ app/src/main/java/com/zedzul/github/hw1_02092017/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //some changes
         Log.d(TAG_GOOD, MSG_ALL_RIGHT);
+        //update master
     }
 
     public void someFileWriter(){
